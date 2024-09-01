a2945232994c0b8800ecbe92f49c6d96eee107a1
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2024-09-01 23:13:53 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 37.25 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    var answer = [];
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] >= 50 && arr[i] % 2 == 0) {
+            answer[i] = arr[i] / 2;
+        } else if(arr[i] < 50 && arr[i] % 2 != 0) {
+            answer[i]  = arr[i] * 2;
+        } else {
+            answer[i]  = arr[i];
+        }
+    }
+    return answer;
+}
