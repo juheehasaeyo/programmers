5c017a7214ac604e100558e063d37c81c740150b
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2025-01-16 16:28:20 +0800

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,33 @@
+function solution(sides) {
+    var answer = 0;
+    if(sides[0] > sides[1]) {
+        if(sides[0] > sides[2]) {
+            if(sides[0] < sides[1] + sides[2]) {
+                answer = 1;
+            } else {
+                answer = 2;
+            }
+        } else {
+            if(sides[2] < sides[0] + sides[1]) {
+                answer = 1;
+            } else {
+                answer = 2;
+            }
+        }
+    } else {
+        if(sides[1] > sides[2]) {
+            if(sides[1] < sides[0] + sides[2]) {
+                answer = 1;
+            } else {
+                answer = 2;
+            }
+        } else {
+            if(sides[2] < sides[0] + sides[1]) {
+                answer = 1;
+            } else {
+                answer = 2;
+            }
+        }
+    }
+    return answer;
+}
