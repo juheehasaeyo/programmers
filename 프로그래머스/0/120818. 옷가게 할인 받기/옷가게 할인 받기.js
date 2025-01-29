a77f52c8583d36c0011c7c4dfb014a9920ee4888
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2025-01-29 17:24:36 +0800

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,13 +1,13 @@
 function solution(price) {
     var answer = 0;
     if (price >= 500000) {
-        answer = Math.floor(0.8 * price);
+        answer = 0.8 * price;
     } else if (price >= 300000) {
-        answer = Math.floor(0.9 * price);
+        answer = 0.9 * price;
     } else if (price >= 100000) {
-        answer = Math.floor(0.95 * price);
+        answer = 0.95 * price;
     } else {
         answer = price;
     }
-    return answer;
+    return parseInt(answer);
 }
