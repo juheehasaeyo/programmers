e367e516e08006f8b73f65c53ad42e19f38126d0
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2025-05-11 23:28:11 +0900

    [level 0] Title: qr code, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -0,0 +1,9 @@
+function solution(q, r, code) {
+    var answer = '';
+    for(var i = 0; i < code.length; i++) {
+        if(i % q == r) {
+            answer += code[i];
+        }
+    }
+    return answer;
+}
