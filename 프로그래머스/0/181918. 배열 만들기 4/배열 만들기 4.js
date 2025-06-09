1dbfc2fbc8cdc4f699347624fac52344f0336677
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2025-06-10 01:23:13 +0900

    [level 0] Title: 배열 만들기 4, Time: 32.09 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    var stk = [];
+    var i = 0;
+    while(i < arr.length) {
+        if(stk.length == 0) {
+            stk.push(arr[i]);
+            i += 1;
+        } else if(stk[stk.length-1] < arr[i]) {
+            stk.push(arr[i]);
+            i += 1;
+        } else {
+            stk.pop();
+        }       
+    }
+    return stk;
+}
