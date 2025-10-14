9b4ba3a9f8dc28f4b03146c340495da591ee9928
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2025-10-14 23:56:33 +0900

    [level 0] Title: 공 던지기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -0,0 +1,11 @@
+function solution(numbers, k) {
+  let idx = 0;
+    
+  for(var i = 1; i < k; i++) {
+    idx += 2;
+    if (idx >= numbers.length) {
+      idx -= numbers.length;
+    }
+  }
+  return numbers[idx];
+}
