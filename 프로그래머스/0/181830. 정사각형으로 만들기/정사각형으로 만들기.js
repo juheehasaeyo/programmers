d5f776ead779fa375357b2271a1b0a188ec9d9e2
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2025-10-22 01:07:46 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.91 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,21 +1,20 @@
 function solution(arr) {
-  const rows = arr.length;
-  const cols = arr[0].length;
-
-  if(rows > cols) {
-    for (let i = 0; i < rows; i++) {
-      while (arr[i].length < rows) {
-        arr[i].push(0);
-      }
-    }
-  } else if(cols > rows) {
-    for (let i = arr.length; i < cols; i++) {
-        const row = [];
-        for (let j = 0; j < cols; j++) {
-            row.push(0);
+    var rows = arr.length;
+    var cols = arr[0].length;
+    if(rows > cols) {
+        for(var i = 0; i < rows; i++) {
+            for(var j = cols; j < rows; j++) {
+                arr[i].push(0);
+            }
         }
-        arr.push(row);
+    } else if(rows < cols) {    
+        for(var i = 0; i < cols - rows; i++) {
+            var newArr = [];
+            for(var j = 0; j < cols; j++) {
+                newArr.push(0);
+            }
+            arr.push(newArr);
+        }
     }
-  }
-     return arr;
+    return arr;
 }
