5d435b21c7d6bf8bdf3ed998d1243768fbc6990c
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2025-11-16 23:09:41 +0900

    [level 0] Title: 소인수분해, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = [];
+    for(var i = 2; i <= n; i++) {
+        if (n % i === 0) {
+            let isPrime = true;
+            for (let j = 2; j * j <= i; j++) {
+                if (i % j === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) answer.push(i);
+        }
+    }
+    return answer;
+}
