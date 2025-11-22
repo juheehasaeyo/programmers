8c8ca60727f6d158c177d1490d72c460b8c44c0c
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2025-11-22 20:57:08 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    var answer = '';
+    
+    for (let i = 0; i < s.length; i++) {
+        if(s[i-1] === ' ' || i === 0) {
+            answer += s[i].toUpperCase();
+        } else {
+            answer += s[i].toLowerCase();
+        }
+    }
+    return answer;
+}
