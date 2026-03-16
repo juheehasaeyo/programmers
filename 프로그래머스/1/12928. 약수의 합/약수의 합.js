8fb199e25ea610fd6c82a86d0d4b9a8f5f1a65ae
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2026-03-16 22:47:57 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,7 +1,7 @@
 function solution(n) {
     var sum = 0;
-    for(var i = 0; i <= n; i++) {
-        if(n % i == 0) {
+    for(let i = 1; i <= n; i++) {
+        if(n % i === 0) {
             sum += i;
         }
     }
