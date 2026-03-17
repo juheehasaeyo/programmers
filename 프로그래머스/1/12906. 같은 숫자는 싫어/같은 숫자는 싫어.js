c0a07c27e1bb1a1c479afc8293462ea5e4bd137a
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2026-03-17 23:41:38 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.55 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,8 +1,8 @@
-function solution(arr)
-{
+function solution(arr) {
     var answer = [];
-    for(var i = 0; i < arr.length; i++) {
-        if(arr[i] !== arr[i+1]) {
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i+1]) {
             answer.push(arr[i]);
         }
     }
