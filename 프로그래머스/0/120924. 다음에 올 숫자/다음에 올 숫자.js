aa8e76a2e20ba32963e0f7f4911a2bca9cc3166a
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2026-03-18 23:22:07 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,11 +1,14 @@
 function solution(common) {
-  var answer = 0;
-  if (common[2] - common[1] === common[1] - common[0]) {
-    const gap = common[1] - common[0];
-    answer = common[common.length - 1] + gap;
-  } else {
-    const ratio = common[1] / common[0];
-    answer = common[common.length - 1] * ratio;
-  }
-  return answer;
+    var answer = 0;
+    for (let i = 0; i < common.length; i++) {
+        var minus = common[i+1] - common[i];
+        var ratio = common[i+1] / common[i];
+        if (minus === common[i+2] - common[i+1]) {
+            answer = common[common.length-1] + minus;
+        }
+        else if (common[i+1] * ratio === common[i+2]) {
+            answer = common[common.length-1] * ratio;
+        }
+    }
+    return answer;
 }
