801551c2db9c6d41764507b7821eccd8dc028c13
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2026-04-24 00:44:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,15 +1,16 @@
 function solution(num) {
-    var answer = 0;
-    while(num != 1) {
-        if (answer >= 500) {
+    var cnt = 0;
+    while (num !== 1) {
+        if (cnt === 500) {
             return -1;
         }
-        if(num % 2 == 0) {
+        if (num % 2 === 0) {
             num = num / 2;
         } else {
-            num = num * 3 + 1;
+            num = num * 3 + 1; 
         }
-        answer++;
+        cnt += 1;
     }
-    return answer;
+    
+    return cnt;
 }
