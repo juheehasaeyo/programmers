7bf2012af1e8b9482c014c18d37412325818771f
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2026-04-28 22:31:54 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -3,5 +3,6 @@ function solution(phone_number) {
     for (let i = phone_number.length; i > 4; i--) {
         answer += '*';
     }
-    return answer + phone_number.slice(-4);
+    answer += phone_number.slice(-4);
+    return answer;
 }
